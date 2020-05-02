0214815c2d6af3b67f3f0a41f6769baac0f8df8c
rabah babaci <4rabah@gmail.com>
2020-05-02 09:42:46 -0700

    finish setting up the login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,7 @@ const initialState = {
   password: '',
 };
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [userInfo, setUserInfo] = useState(initialState);
@@ -18,39 +18,41 @@ const Login = () => {
   };
 
   return (
-    <div className='login-form'>
-      <h1>Welcome to the Bubble App!</h1>
+    <>
+      <div className='login-form'>
+        <h1>Welcome to the Bubble App!</h1>
 
-      <h1>Log in</h1>
+        <h1 className='login-header'>Log in</h1>
 
-      <form>
-        <label>
-          Type your username:
-          <input
-            onChange={handleChange}
-            type='text'
-            name='username'
-            placeholder='...username'
-            required
-          />
-        </label>
+        <form onSubmit={handleLogin}>
+          <label>
+            Type your username:
+            <input
+              onChange={handleChange}
+              type='text'
+              name='username'
+              placeholder='...username'
+              required
+            />
+          </label>
 
-        <label>
-          Type your password:
-          <input
-            onChange={handleChange}
-            type='text'
-            name='password'
-            placeholder='...password'
-            required
-          />
-        </label>
+          <label>
+            Type your password:
+            <input
+              onChange={handleChange}
+              type='text'
+              name='password'
+              placeholder='...password'
+              required
+            />
+          </label>
 
-        <section className='btn-section'>
-          <button>Login</button>
-        </section>
-      </form>
-    </div>
+          <section className='btn-section'>
+            <button>Login</button>
+          </section>
+        </form>
+      </div>
+    </>
   );
 };
 
