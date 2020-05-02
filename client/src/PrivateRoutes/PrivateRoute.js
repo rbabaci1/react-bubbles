82d7a29b4957eb0e5a9392ff03bc74ac4abdda0a
rabah babaci <4rabah@gmail.com>
2020-05-02 10:22:38 -0700

    implement the PrivateRoute to route to BubblePage if token is available only

--- client/src/PrivateRoutes/PrivateRoute.js
+++ client/src/PrivateRoutes/PrivateRoute.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-export default function PrivateRoute() {
-  return <div></div>;
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        localStorage.getItem('token') ? <Component /> : <Redirect to='/' />
+      }
+    />
+  );
 }
