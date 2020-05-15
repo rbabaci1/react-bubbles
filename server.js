191adf9bc2eada9f1f27e9492dc3ed06becc6896
rabah babaci <4rabah@gmail.com>
2020-05-14 21:04:48 -0700

    test and update all enpoints

--- server.js
+++ server.js
@@ -101,8 +101,9 @@ function authenticator(req, res, next) {
   }
 }
 
-app.post("/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
+
   if (username === "username" && password === "password") {
     req.loggedIn = true;
     setTimeout(() => {
@@ -117,11 +118,11 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.get("/colors", authenticator, (req, res) => {
+app.get("/api/colors", authenticator, (req, res) => {
   res.send(colors);
 });
 
-app.post("/colors", authenticator, (req, res) => {
+app.post("/api/colors", authenticator, (req, res) => {
   if (req.body.color !== undefined && req.body.code !== undefined) {
     const newcolor = req.body;
     newcolor.id = nextId;
@@ -131,7 +132,7 @@ app.post("/colors", authenticator, (req, res) => {
   res.status(201).json(colors);
 });
 
-app.put("/colors/:id", authenticator, (req, res) => {
+app.put("/api/colors/:id", authenticator, (req, res) => {
   if (!req.params.id)
     res.status(400).send("Your request is missing the color id");
   if (req.body.id === undefined || !req.body.color || !req.body.code) {
@@ -148,7 +149,7 @@ app.put("/colors/:id", authenticator, (req, res) => {
   res.status(200).send(req.body);
 });
 
-app.delete("/colors/:id", authenticator, (req, res) => {
+app.delete("/api/colors/:id", authenticator, (req, res) => {
   if (!req.params.id)
     res.status(400).send("Your request is missing the color id");
   colors = colors.filter(color => `${color.id}` !== req.params.id);
@@ -160,5 +161,5 @@ app.get("/", function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log("Server listening on port 5000");
+  console.log(`Server listening on port ${port}`);
 });
